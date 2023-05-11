b2306f23ccabb17611a42815bed294eee6d90fdf
Hebi Li <lihebi.com@gmail.com>
2023-05-11 01:44:48 -0700

    try link brings you to home instead of login page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,7 +45,7 @@ export function Hero() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="https://app.codepod.io/login"
+                  href="https://app.codepod.io"
                   target="_blank"
                   className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
